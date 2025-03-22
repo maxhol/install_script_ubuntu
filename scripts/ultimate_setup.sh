a5bbf2eb2b7c28ca2add7c2fb363d3ee97bab592
Max <maxhol@hotmail.fr>
2025-03-22 12:24:33 +0100

    add lazygit

--- scripts/ultimate_setup.sh
+++ scripts/ultimate_setup.sh
@@ -52,8 +52,13 @@ else
   echo "Alias already exists in .zshrc"
 fi
 
-
-
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazigit
+rm lazygit.tar.gz 
 
 # Install ROS2 and dependancies
 apt install -y curl gnupg2 lsb-release software-properties-common
